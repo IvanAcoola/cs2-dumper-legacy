66f8e44add6d5a8d1515389fa4c3854a6cf75634
Ivan Cherepovec <80287522+IvanAcoola@users.noreply.github.com>
2024-03-28 18:53:19 +0200

    init

---
+++ generated/networksystem.dll.hpp
@@ -0,0 +1,12 @@
+/*
+ * Generated using https://github.com/a2x/cs2-dumper
+ * Sat, 23 Mar 2024 05:02:03 +0000
+ */
+
+#pragma once
+
+#include <cstddef>
+
+namespace ChangeAccessorFieldPathIndex_t {
+    constexpr std::ptrdiff_t m_Value = 0x0; // int16_t
+}
